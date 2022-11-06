218c446fb34062e813e2524ab0c0f03f8370701f
@Slava-Fly <school.94@bk.ru>
2022-11-06 19:46:38 +0800

    Initial Commit

---
+++ UserAuthorizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserAuthorizationApp
+//
+//  Created by User on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
